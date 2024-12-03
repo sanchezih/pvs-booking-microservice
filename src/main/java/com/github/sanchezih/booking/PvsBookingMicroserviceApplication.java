8faf04166e2cf0125ba29128242c39f89559b51e
Ignacio Sanchez <sanchezih@gmail.com>
2024-12-03 15:49:35 -0300

    inicial

---
+++ src/main/java/com/github/sanchezih/booking/PvsBookingMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.github.sanchezih.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PvsBookingMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PvsBookingMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/sanchezih/booking/PvsBookingMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sanchezih.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PvsBookingMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
