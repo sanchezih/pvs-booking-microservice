c7503272c1101587d441bc8be139bed4838d11b4
Ignacio Sanchez <sanchezih@gmail.com>
2024-12-04 23:08:50 -0300

    update

--- src/main/java/com/github/sanchezih/booking/Application.java
+++ src/main/java/com/github/sanchezih/booking/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PvsBookingMicroserviceApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PvsBookingMicroserviceApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
